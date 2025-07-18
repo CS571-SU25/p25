f3d5e3930ef60b20b29a371de60389a63e6e216e
Nick Kelly <nekelly12@gmail.com>
2025-07-18 16:12:26 -0500

    third

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Players from './components/Players'
 import Home from './components/Home'
 import Header from './components/Header'
 import Schedule from './components/Schedule'
+import Tickets from './components/Tickets'
+import Stats from './components/Stats'
+import Jerseys from './components/Jerseys'
 
 function App() {
   return <HashRouter>
@@ -15,6 +18,9 @@ function App() {
       <Route path="/" element={<Home />}></Route>
       <Route path="/player-info" element={<Players/>}></Route>
       <Route path="/schedule" element={<Schedule/>}></Route>
+      <Route path="/tickets" element={<Tickets/>}></Route>
+      <Route path="/stats" element={<Stats/>}></Route>
+      <Route path="/jerseys" element={<Jerseys/>}></Route>
     </Routes>
   </HashRouter>
 }
--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,8 +1,17 @@
 
+import { Link } from 'react-router-dom';
+
 export default function Players() {
 
     return <div>
         <h1>Meet the Team!</h1>
+        <img width="750" src="https://images.sidearmdev.com/convert?url=https%3A%2F%2Fdxbhsrqyrr690.cloudfront.net%2Fsidearm.nextgen.sites%2Fuwbadgers.com%2Fimages%2F2025%2F3%2F8%2F250308MBB-2265.jpg&type=webp"></img>
+        <h2>
+            <Link to="/stats">Click here to check player stats</Link>
+        </h2>
+        <h2>
+            <Link to="/jerseys">Click here to view jerseys</Link>
+        </h2>
         {/* Player list will go here */}
     </div>
     
