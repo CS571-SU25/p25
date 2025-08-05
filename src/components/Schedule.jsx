811e9aaf2ca19a45f2e9a9e41ea2b813bfbdd72f
Nick Kelly <nekelly12@gmail.com>
2025-08-04 20:14:46 -0400

    sixth

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,7 +4,7 @@ export default function Schedule() {
 
     return <div>
         <h1>
-            <Link to="/tickets">Click here to purchase tickets</Link>
+            <Link to="/tickets">Click Here to Purchase Tickets</Link>
         </h1>
         <img width="750" src="https://images.sidearmdev.com/convert?url=https%3A%2F%2Fdxbhsrqyrr690.cloudfront.net%2Fsidearm.nextgen.sites%2Fuwbadgers.com%2Fimages%2F2024%2F5%2F1%2F2024-25_BigTenHomeAwayHeadline.jpg&type=webp"></img>
     </div>
